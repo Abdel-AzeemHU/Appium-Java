5abc384cccf1be5494f0b9029abd908fc16d13d6
amado-saladino <salaheldin.ahmed1@gmail.com>
2019-07-22 00:53:22 +0200

    modify TestCase and adding Helper Class

--- src/test/java/tests/TestCase.java
+++ src/test/java/tests/TestCase.java
@@ -6,8 +6,11 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import utilities.Helper;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -30,6 +33,15 @@ public class TestCase {
 
         AppiumDriver<MobileElement> driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void screenshotOnFailure(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            System.out.println("Failed!");
+            System.out.println("Taking Screenshot....");
+            Helper.captureScreenshot(driver, result.getName());
+        }
     }
 
     @AfterSuite
