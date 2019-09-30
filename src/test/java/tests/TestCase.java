50da99950c71a324ecbc0b0f7763a6b3c417ad51
amado-saladino <salaheldin.ahmed1@gmail.com>
2019-09-30 20:56:08 +0200

    browserstack config

--- src/test/java/tests/TestCase.java
+++ src/test/java/tests/TestCase.java
@@ -19,11 +19,13 @@ import java.util.concurrent.TimeUnit;
 public class TestCase {
 
     protected AndroidDriver driver;
+    public static String userName = "testuser1114";
+    public static String accessKey = "xBHYQ1ssVGrkK2w66ZYC";
 
     @BeforeSuite
     public void startDriver() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        /*capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9");
         //capabilities.setCapability(MobileCapabilityType.APP, "/path/to/.apk/file");
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Redmi device");
@@ -31,7 +33,14 @@ public class TestCase {
         capabilities.setCapability("appPackage","io.selendroid.testapp");
         capabilities.setCapability("appActivity",".HomeScreenActivity");
 
-        driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+        driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);*/
+        capabilities.setCapability("device", "Samsung Galaxy Note 8");
+        capabilities.setCapability("os_version", "7.1");
+        capabilities.setCapability("name", "TestApp");
+        capabilities.setCapability("app", "bs://b0d911da703aede113c340d8438ce891d3288c17");
+        capabilities.setCapability("browserstack.debug", true);
+        driver = new AndroidDriver(new URL("https://"+userName+":"+accessKey+"@hub-cloud.browserstack.com/wd/hub"), capabilities);
+
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
